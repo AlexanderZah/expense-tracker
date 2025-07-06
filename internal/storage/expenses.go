8a9e74ce2e63b66b379ce28151f0beddd2ec4851
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2025-07-06 22:33:19 +0300

    init git

---
+++ internal/storage/expenses.go
@@ -0,0 +1,77 @@
+package storage
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+type Expense struct {
+	ID          int     `json:"id"`
+	Date        string  `json:"date"`
+	Description string  `json:"description"`
+	Amount      float64 `json:"amount"`
+}
+
+func loadExpenses(filename string) ([]Expense, error) {
+	var expenses []Expense
+
+	file, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return expenses, nil
+		}
+		return nil, err
+	}
+
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) == 0 {
+		return []Expense{}, nil // пустой список, если файл пуст
+	}
+
+	err = json.Unmarshal(data, &expenses)
+	if err != nil {
+		return nil, err
+	}
+
+	return expenses, nil
+}
+
+func saveExpenses(filename string, expenses []Expense) error {
+	data, err := json.MarshalIndent(expenses, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func AddExpense(filename, description string, amount float64) (int, error) {
+	expenses, err := loadExpenses(filename)
+	if err != nil {
+		return 0, err
+	}
+
+	newID := 1
+	if len(expenses) > 0 {
+		newID = expenses[len(expenses)-1].ID + 1
+	}
+
+	newExpense := Expense{
+		ID:          newID,
+		Date:        time.Now().Format(time.RFC3339),
+		Description: description,
+		Amount:      amount,
+	}
+
+	expenses = append(expenses, newExpense)
+
+	return newID, saveExpenses(filename, expenses)
+}
